80d7d653b705c19e448d94188455a0ab62742463
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-02-13 00:33:07 -0500

    Create WordPair.java

---
+++ lessons/ArrayList/practice/WordPair.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class WordPairTest {
+     public static void main(String[] args)
+     {
+         // Create an ArrayList of WordPair objects called pairs
+
+
+         pairs.add(new WordPair("hi","there"));
+         pairs.add(new WordPair("hi","bye"));
+         System.out.println(pairs);
+     }
+ }
+
+ class WordPair {
+     private String word1;
+     private String word2;
+
+     public WordPair(String w1, String w2) {
+         word1 = w1;
+         word2 = w2;
+     }
+     public String getFirst() {
+         return word1;
+     }
+     public String getSecond() {
+         return word2;
+     }
+     public String toString() {
+         return "(" + word1 + ", " + word2 + ")";
+     }
+ }
+ 
