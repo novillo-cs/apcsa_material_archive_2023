5e96548220e3050e40bf757cf411de6acfcc3221
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-03-08 00:28:10 -0500

    Create Driver.java

---
+++ lessons/linked_list/Driver.java
@@ -0,0 +1,11 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+  public static void main(String[] args) {
+    MyLinkedList myList = new MyLinkedList();
+    
+    System.out.println(l);
+  }
+}
