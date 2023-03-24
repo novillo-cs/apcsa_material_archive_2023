86c1b04107201f8e922cf0ce5231c165213d036e
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-03-23 23:47:59 -0400

    Update Queue.java

--- lessons/stacks_queues/Queue.java
+++ lessons/stacks_queues/Queue.java
@@ -1,1 +1,45 @@
+public class Queue{
+  // This Queue class is designed for integer values >= 0
+  
+  // Place private instance variables here 
 
+  // Place constructors here
+  public Queue(){
+
+  }
+
+  
+  // Enqueue a new value to the queue
+  public void enqueue(int value){
+
+  }
+
+   
+  // Dequeue a value from the queue
+  public int dequeue(){
+
+  }
+
+  // Return the front/top
+  public int front(){
+
+  }
+
+
+  // Check is queue is empty
+  public boolean isEmpty(){
+
+  }
+
+  // Return queue size
+  public int size(){
+
+  }
+
+  
+  // Print queue
+  public String toString(){
+
+  }
+
+}
