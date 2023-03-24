73b77cd106b961b4772ff69c17d6648dc2ecb224
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-03-23 23:52:45 -0400

    Update Stack.java

--- lessons/stacks_queues/Stack.java
+++ lessons/stacks_queues/Stack.java
@@ -1,1 +1,44 @@
 
+public class Stack{
+  // This Stack class is designed for integer values >= 0
+
+  // Instance variables here
+
+ // COnstructor
+  public Stack(){
+
+  }   
+
+  // Add element to stack
+  public void push(int value){
+
+  }
+
+  // Remove element from stack  
+  public int pop(){
+
+  }
+
+  // Get top element
+  public int top(){
+
+  }
+
+  // Check is stack empty
+  public boolean isEmpty(){
+
+  }
+
+  // Return size stack
+  public int size(){
+  
+  }
+
+  // Print stack
+  public String toString(){
+
+  }
+
+
+}
+
