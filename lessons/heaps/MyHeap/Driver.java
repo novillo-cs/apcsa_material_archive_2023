1608307568a42880c2c4df361a4eb1145a3d08a2
novillo-cs <123229891+novillo-cs@users.noreply.github.com>
2023-05-12 01:03:10 -0400

    Update Driver.java

--- lessons/heaps/MyHeap/Driver.java
+++ lessons/heaps/MyHeap/Driver.java
@@ -1,20 +1,19 @@
-public class Driver {
+public class Driver{
 
-    public static void main(String[] args) {
-        MyHeap heap = new MyHeap(10);
+    public static void main(String[] arg)
+    {
+      	// This is a max heap
+        MyHeap heap = new MyHeap(14);
+        heap.insert(10);
+        heap.insert(7);
+        heap.insert(26);
+        heap.insert(8);
+        heap.insert(1);
+        heap.insert(20);
+        heap.insert(15);
 
-        // insert more values into heap
-        heap.insert(80);
-
-        heap.printHeap();
-
-        System.out.println(heap.peek());
-
-        // delete some items from the heap
-        heap.delete(0);
-        heap.printHeap();
-
-        System.out.println(heap.peek());
-
+        heap.print();
+        System.out.println("The deleted value is " + heap.deleteRoot());
+        heap.print();
     }
 }
